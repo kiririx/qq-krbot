2771901426faacf3868d3d065d55e9068605d3dc
キリリ <kiriri_ri@126.com>
2022-06-26 22:26:02 +0800

    feat: pixiv

---
+++ handler/pixiv.go
@@ -0,0 +1,182 @@
+package handler
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"github.com/kiririx/krutils/algo_util"
+	"github.com/kiririx/krutils/http_util"
+	"github.com/kiririx/krutils/json_util"
+	"io"
+	"os"
+	"path"
+	"regexp"
+	"time"
+)
+
+var (
+	client_id      = "MOBrBDS8blbauoSck0ZfDbtuzpyT"
+	client_secret  = "lsACyCD94FhDUtGTXi3QzcFE2uU1hqtDaKeqrdwj"
+	hash_secret    = "28c1fdd170a5204386cb1313c7077b34f83e4aaf4aa829ce78c231e05b0bae2c"
+	base_hosts     = "https://app-api.pixiv.net"
+	_REFRESH_TOKEN = "vE94AY1QvM8BGcJuA6o1lPFBpfwv8YrDeuaH1AQWZRQ"
+	AccessToken    = ""
+	proxyUrl       = "http://127.0.0.1:7890"
+	ExpireTime     int64
+)
+
+type PixivClient struct {
+	Headers map[string]string
+}
+
+func GetHeaders() map[string]string {
+	localTime := time.Now().Format(time.RFC3339)
+	px := make(map[string]string)
+	px["Accept-Language"] = "en-us"
+	px["X-Client-Time"] = localTime
+	px["X-Client-Hash"] = genClientHash(localTime)
+	px["User-Agent"] = "PixivAndroidApp/5.0.115 (Android 6.0)"
+	if AccessToken == "" {
+		Auth()
+	}
+	px["Authorization"] = "Bearer " + AccessToken
+	return px
+}
+
+func DownloadImg(url string) (string, error) {
+	referer := "https://app-api.pixiv.net/"
+	ext, _ := GetFileExt(url)
+	resp, err := http_util.Client(time.Second * 20).Proxy(proxyUrl).Headers(map[string]string{
+		"Referer": referer,
+	}).Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+	fileName := algo_util.UUID() + "." + ext
+	f, err := os.Create("./photo/" + fileName)
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+	io.Copy(f, resp.Body)
+	f.Close()
+	return fileName, nil
+}
+
+// GetFileExt 获取文件的扩展名
+func GetFileExt(fileAddr string) (string, error) {
+	fileName := path.Base(fileAddr)
+	reg, err := regexp.Compile("\\.(" + "jpg|png|jpeg|JPG|JPEG|PNG" + ")")
+	if err != nil {
+		return "", err
+	}
+	matchedExtArr := reg.FindAllString(fileName, -1)
+	if matchedExtArr != nil && len(matchedExtArr) > 0 {
+		ext := matchedExtArr[len(matchedExtArr)-1]
+		return ext[1:], nil
+	}
+	return "", errors.New("获取文件扩展名失败")
+}
+func Auth() {
+	if AccessToken != "" && time.Now().Unix() < ExpireTime {
+		return
+	}
+	localTime := time.Now().Format(time.RFC3339)
+	px := PixivClient{}
+	px.Headers = make(map[string]string)
+	px.Headers["Accept-Language"] = "en-us"
+	px.Headers["X-Client-Time"] = localTime
+	px.Headers["X-Client-Hash"] = genClientHash(localTime)
+	px.Headers["User-Agent"] = "PixivAndroidApp/5.0.115 (Android 6.0)"
+	json, err := http_util.Client(time.Second*10).
+		Proxy("http://127.0.0.1:7890").
+		PostFormGetJSON("https://oauth.secure.pixiv.net"+"/auth/token", map[string]string{
+			"client_id":      client_id,
+			"client_secret":  client_secret,
+			"grant_type":     "refresh_token",
+			"get_secure_url": "1",
+			"refresh_token":  _REFRESH_TOKEN,
+		})
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	AccessToken = json["access_token"].(string)
+	ExpireTime = time.Now().Unix() + int64(json["expires_in"].(float64))
+}
+
+func genClientHash(clientTime string) string {
+	h := md5.New()
+	io.WriteString(h, clientTime)
+	io.WriteString(h, hash_secret)
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Rank() []string {
+	host := "https://app-api.pixiv.net"
+	url := host + "/v1/illust/ranking"
+	mode := "day_male_r18"
+	filter := "for_ios"
+	json, err := http_util.Client(time.Second*3).Proxy(proxyUrl).Headers(GetHeaders()).GetJSON(url, map[string]string{
+		"mode":   mode,
+		"filter": filter,
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	photos := make([]string, 0)
+	m, err := json_util.JSON2Map(json)
+	illusts := m["illusts"].([]interface{})
+	for _, illust := range illusts {
+		image := illust.(map[string]interface{})["image_urls"].(map[string]interface{})
+		if image["large"] != "" {
+			photos = append(photos, image["large"].(string))
+		} else if image["medium"] != "" {
+			photos = append(photos, image["medium"].(string))
+		} else if image["square_medium"] != "" {
+			photos = append(photos, image["square_medium"].(string))
+		}
+	}
+	return photos
+}
+
+func Recommend() (map[string]interface{}, error) {
+	req := base_hosts + "/v1/illust/recommended"
+	headers := GetHeaders()
+	headers[`include_ranking_label`] = "true"
+	json, err := http_util.Client(time.Second*3).Proxy(proxyUrl).Headers(headers).GetJSON(req, map[string]string{})
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	j, err := json_util.JSON2Map(json)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	return j, nil
+}
+
+func Search(tag string) (map[string]interface{}, error) {
+	url := base_hosts + "/v1/search/illust"
+	headers := GetHeaders()
+	json, err := http_util.Client(time.Second*10).Proxy(proxyUrl).Headers(headers).GetJSON(url, map[string]string{
+		"word":          tag,
+		"search_target": "partial_match_for_tags",
+		"sort":          "popular_desc",
+		"filter":        "for_ios",
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	j, err := json_util.JSON2Map(json)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	return j, nil
+}
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/kiririx/krutils/algo_util"
 	"github.com/kiririx/krutils/http_util"
 	"github.com/kiririx/krutils/map_util"
 	"github.com/kiririx/krutils/str_util"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"qq-krbot/handler"
@@ -15,6 +18,7 @@ import (
 var (
 	LangReflect map[string]int
 	BaseReflect map[string]string
+	AnimeImages []string
 )
 
 func init() {
@@ -35,6 +39,15 @@ func init() {
 	BaseReflect["早上好"] = "早上好哦🌷️(◍•ᴗ•◍)ﾉ"
 	BaseReflect["中午好"] = "中午好哦🌷️(◍•ᴗ•◍)ﾉ"
 	BaseReflect["晚上好"] = "晚上好哦🌷️(◍•ᴗ•◍)ﾉ"
+	// 涩图列表
+	AnimeImages = make([]string, 0)
+	files, _ := ioutil.ReadDir("./photo")
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		AnimeImages = append(AnimeImages, file.Name())
+	}
 }
 
 func main() {
@@ -65,16 +78,52 @@ func main() {
 				trans(param.Message, param.GroupId, LangReflect[prefix])
 				return
 			}
+			if str_util.Prefix(param.Message, "#") {
+				tag := str_util.SubStr(param.Message, 1, -1)
+				m, err := handler.Search(tag)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				fmt.Println(m)
+				photos := make([]string, 0)
+				illusts := m["illusts"].([]interface{})
+				for _, illust := range illusts {
+					image := illust.(map[string]interface{})["meta_single_page"].(map[string]interface{})
+					var imgUrl = image["original_image_url"]
+					if imgUrl != nil {
+						photos = append(photos, imgUrl.(string))
+					} else {
+						images := illust.(map[string]interface{})["meta_pages"].([]interface{})
+						for _, v := range images {
+							img := v.(map[string]interface{})
+							url := img["image_urls"].(map[string]interface{})["original"]
+							if url != nil {
+								photos = append(photos, url.(string))
+							}
+						}
+					}
+				}
+				p := photos[algo_util.RandomInt(0, len(photos))]
+				imgName, err := handler.DownloadImg(p)
+				if err != nil {
+					Error(err, param.GroupId)
+					return
+				}
+				sendMsg := fmt.Sprintf("[CQ:image,file=http://127.0.0.1:10011/photo/%v,subType=0]", imgName)
+				sendToGroup(param.GroupId, sendMsg)
+				return
+			}
 			// 判断是否是@消息
 			if param.CQ == "at" {
 				var msg string
 				if str_util.Contains(param.Message, "ヘルプ", "帮助", "help", "?", "？") {
 					msg = "🌸リカちゃんの使う方🌸\n" +
 						"1. 翻訳：『日中　おはようございます』" + "\n" +
-						"2. エロな絵（開発中）" + "\n" +
+						"2. エロな絵: 『# 可可萝』（# searchKey）を送信する" + "\n" +
 						"3. 天気予報（開発中）" + "\n" +
 						"4. AIリプ（開発中）" + "\n" +
-						"5. DNFゴールド：『ゴールド』『金币』『比例』を送信する" + "\n" +
+						"5. DNFゴールド:『ゴールド』『金币』『比例』を送信する" + "\n" +
 						"6. ......" + "\n"
 				} else if str_util.Contains(param.Message, "DNF", "dnf", "ゴールド", "金币", "比例") {
 					glod, err := handler.DNFHandler().Gold()
@@ -91,6 +140,11 @@ func main() {
 					if len(keys) > 0 {
 						msg = BaseReflect[keys[0]]
 					}
+				} else if str_util.Contains(param.Message, "色图", "涩图") {
+					fileName := AnimeImages[algo_util.RandomInt(0, len(AnimeImages))]
+					sendMsg := fmt.Sprintf("[CQ:image,file=http://127.0.0.1:10011/photo/%v,subType=0]", fileName)
+					sendToGroup(param.GroupId, sendMsg)
+					return
 				} else {
 					msg = "何か用事がありますか。"
 				}
@@ -103,6 +157,8 @@ func main() {
 			}
 		}
 	})
+	r.Use(static.Serve("/photo", static.LocalFile("./photo", true)))
+
 	r.Run(":10011") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
@@ -116,10 +172,15 @@ func trans(text string, groupId int64, transType int) {
 
 func sendToGroup(groupId int64, msg string) {
 	url := CqHttp + "/send_group_msg"
-	_, _ = http_util.Client(time.Second*4).PostJSON(url, map[string]any{
+	resp, err := http_util.Client(time.Second*10).PostJSON(url, map[string]any{
 		"group_id": str_util.ToStr(groupId),
 		"message":  msg,
 	})
+	if err != nil {
+		Error(err, groupId)
+		return
+	}
+	fmt.Println(resp)
 }
 
 func atToGroup(groupId, userId int64, msg string) {
